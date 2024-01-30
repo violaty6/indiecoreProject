8eb5141ce8f3621fc4a1492b1957e30412d2c4a9
violaty6 <violaty9510@gmail.com>
2024-01-30 14:55:20 +0300

    ProjectOpen

---
+++ Indiecore/Assets/_Script/Card.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card", menuName = "ScriptableObjects/Card", order = 1)]
+public class Card : ScriptableObject
+{
+    public string cardName;
+    
+
+}
+
+
+//Eğer buraya kadar geldiysen iyi bir yazılımcısın demektir... bu elindeki güç seni şaha da kaldırır, seni yere de batırır.
+//Kulak verin bu dediklerime hayat bazen karttan ibaret değil midir ?...
